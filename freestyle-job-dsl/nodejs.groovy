33167ba99a311651530da27ae65c20e573d190ed
Maina, Stephen <m41na@yahoo.com>
2021-10-16 19:52:52 -0500

    updating dsl script

--- freestyle-job-dsl/nodejs.groovy
+++ freestyle-job-dsl/nodejs.groovy
@@ -1,3 +1,18 @@
 job('Node js example') {
-    scm()
+    scm('https://github.com/m41na/jenkins-job-dsl.git') { node ->
+        node / gitConfigName('DSL User')
+        node /gitConfigEmail('jenkins-admin@aol.com')
+    }
+
+    triggers {
+        scm('H/5 * * * *')
+    }
+
+    wrapper{
+        nodejs('node16')
+    }
+
+    steps {
+        shell('npm i')
+    }
 }
